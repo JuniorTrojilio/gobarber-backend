ffd5e65483c3509cedfe4f84e1cbd66dded45941
Junior Trojilio <osmar.trojilio@gmail.com>
2020-07-21 17:39:53 -0300

    Create upload of images

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
 import CreateUserService from '../services/createUser.services';
+import UpdateUserAvatarService from '../services/updateUserAvatar.services';
+import ensureAuthenticated from '../middlewares/ensureAuthenticaded';
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
     try {
@@ -20,5 +25,26 @@ usersRouter.post('/', async (request, response) => {
         return response.status(400).json({ error: err.message });
     }
 });
+
+usersRouter.patch(
+    '/avatar',
+    ensureAuthenticated,
+    upload.single('avatar'),
+    async (request, response) => {
+        try {
+            const updateUserAvatar = new UpdateUserAvatarService();
+
+            const user = await updateUserAvatar.execute({
+                user_id: request.user.id,
+                avatarFileName: request.file.filename,
+            });
+
+            delete user.password;
+            return response.json(user);
+        } catch (err) {
+            return response.status(400).json({ error: err.message });
+        }
+    },
+);
 
 export default usersRouter;
